385e9d482162a621b051e677b883523fa0d68bae
Joni <jonathan.tanasa7@gmail.com>
2024-02-25 20:03:26 +0100

    cambios proyecto

--- routes/empresas.js
+++ routes/empresas.js
@@ -13,7 +13,7 @@ const verificarToken = require('../middlewares/verificarToken');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       // Especificar la carpeta donde se guardarán los archivos subidos
-      cb(null, __dirname + '/uploads/images');
+      cb(null, path.join(__dirname, '..', 'uploads', 'images'));
     },
     filename: function (req, file, cb) {
       // Generar un nombre único utilizando la función uuid
